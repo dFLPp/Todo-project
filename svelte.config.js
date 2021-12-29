fe548c428e6e5faa3a549737e1305543ed8c1687
Felipe Castro <felipdsousc@gmail.com>
2021-12-29 22:37:46 +0000

    triying to solve vite conflict

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,10 @@ const config = {
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		vite:{
-			server:{
-				hmr:{
-					clientPort: process.env.HMR_HOST ? 433 : 3000,
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443: 3000,
 					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
 				}
 			}

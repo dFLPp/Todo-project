dfe744eb83d6cf27a42ae03deb2f2aaef33544db
Felipe Castro <felipdsousc@gmail.com>
2021-12-30 18:16:58 +0000

    html skeleton

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="check-state" value="">
+        <button aria-label="check done/ not done">check status</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text">
+        <button aria-label="save item">save button</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="delete item">delete button</button>
+    </form>
+</div>
